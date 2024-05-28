b2fff063056e4582dfe32a923e43897956891407
dinesh <dinesh.m@edvoy.com>
2024-05-28 13:56:02 +0530

    contact and our team

--- app.vue
+++ app.vue
@@ -5,47 +5,47 @@
 </template>
 
 <script setup lang="ts">
-const firebaseAuth = useFirebaseAuth();
-const user = useCurrentUser();
-const router = useRouter();
+const firebaseAuth = useFirebaseAuth()
+const user = useCurrentUser()
+const router = useRouter()
 
-const idToken = ref('');
+const idToken = ref('')
 
 const refreshIdToken = async () => {
-  const accessToken = useCookie('accessToken');
+  const accessToken = useCookie('accessToken')
   if (firebaseAuth?.currentUser) {
     try {
-      const newIdToken = await user.value?.getIdToken(true);
-      accessToken.value = newIdToken;
+      const newIdToken = await user.value?.getIdToken(true)
+      accessToken.value = newIdToken
     } catch (error) {
-      console.error('Error refreshing ID token:', error);
+      console.error('Error refreshing ID token:', error)
     }
   }
-};
+}
 
 onMounted(() => {
   firebaseAuth?.onAuthStateChanged(async (user) => {
-    const accessToken = useCookie('accessToken');
-    const initialIdToken = await user?.getIdToken();
-    accessToken.value = initialIdToken;
-    const tokenRefreshInterval = setInterval(refreshIdToken, 900000);
-    onUnmounted(() => {
-      clearInterval(tokenRefreshInterval);
-    });
-  });
-});
+    const accessToken = useCookie('accessToken')
+    const initialIdToken = await user?.getIdToken()
+    accessToken.value = initialIdToken
+    // const tokenRefreshInterval = setInterval(refreshIdToken, 900000);
+    // onUnmounted(() => {
+    //   clearInterval(tokenRefreshInterval)
+    // })
+  })
+})
 
 watch(
   user,
   async () => {
-    const _user = await getCurrentUser();
+    const _user = await getCurrentUser()
     if (!_user) {
-      router.push('/login');
+      router.push('/login')
     } else if (_user) {
       // const { refetch: fetchuser } = useUser();
       // await fetchuser();
     }
   },
   { immediate: true }
-);
+)
 </script>
--- pages/our-team/index.vue
+++ pages/our-team/index.vue
@@ -1,3 +1,174 @@
 <template>
-  <div>Our team</div>
+  <div class="bg-white py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl lg:mx-0">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Our team
+        </h2>
+        <p class="mt-6 text-lg leading-8 text-gray-600">
+          We’re a dynamic group of individuals who are passionate about what we
+          do and dedicated to delivering the best results for our clients.
+        </p>
+      </div>
+      <ul
+        role="list"
+        class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+      >
+        <li v-for="person in people" :key="person.name">
+          <img
+            class="aspect-[3/2] w-full rounded-2xl object-cover"
+            :src="person.imageUrl"
+            alt=""
+          />
+          <h3
+            class="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900"
+          >
+            {{ person.name }}
+          </h3>
+          <p class="text-base leading-7 text-gray-600">{{ person.role }}</p>
+          <ul role="list" class="mt-6 flex gap-x-6">
+            <li>
+              <a
+                :href="person.linkedinUrl"
+                class="text-gray-400 hover:text-gray-500"
+              >
+                <span class="sr-only">LinkedIn</span>
+                <svg
+                  class="h-5 w-5"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
+
+Employees:
+
+<script setup>
+const people = [
+  {
+    name: 'Guhan Gunasekaran',
+    role: 'Director & Founder',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'R. Viramuthu',
+    role: 'Director & CoFounder',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'Sayed Jamaluddin',
+    role: 'Director',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'Prof N. Ramesh Babu',
+    role: 'Faculty advisor',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'Prof Satyanarayanan Seshadri',
+    role: 'Faculty advisor',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'Prof. Sathyan Subbiah',
+    role: 'Faculty advisor',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'Prof. Muruganandam T M',
+    role: 'Faculty advisor',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'Srikanth R',
+    role: 'Chief Technical Officer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'Saminathan G',
+    role: 'Chief Financial Officer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'G Krishnan',
+    role: 'Senior Technician',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'J Purushothaman',
+    role: 'Junior Executive Engineer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'Nani Yegireddi',
+    role: 'Trainee Engineer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'Babu',
+    role: 'Trainee Engineer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'Vignesh',
+    role: 'Trainee Engineer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'Fiaz',
+    role: 'Design Engineer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  },
+  {
+    name: 'Karthik G',
+    role: 'Marketing / HR',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    linkedinUrl: '#'
+  }
+]
+</script>
