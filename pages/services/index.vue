b2f27e5a5fd0f46ae88fe898fc8e4d7d20728a11
dinesh <dinesh.m@edvoy.com>
2024-05-28 21:09:01 +0530

    services page

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,3 +1,89 @@
 <template>
-  <div>Services page</div>
+  <div class="bg-white">
+    <div
+      class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8"
+    >
+      <div class="mx-auto max-w-2xl lg:mx-0">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Services and support
+        </h2>
+        <p class="mt-6 text-lg leading-8 text-gray-600">
+          We provide world-class services and support with our wide-spread
+          expertise in various fields
+        </p>
+      </div>
+
+      <div
+        class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 xl:gap-x-8 mt-6"
+      >
+        <div v-for="product in products" :key="product.id" class="group">
+          <div
+            class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7"
+          >
+            <img
+              :src="product.imageSrc"
+              :alt="product.imageAlt"
+              class="h-full w-full object-cover object-center group-hover:opacity-75"
+            />
+          </div>
+          <h3 class="mt-4 text-gray-700 text-center text-lg">
+            {{ product.name }}
+          </h3>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+const products = [
+  {
+    id: 1,
+    name: '3D modelling and CAD design',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg',
+    imageAlt:
+      'Tall slender porcelain bottle with natural clay textured body and cork stopper.'
+  },
+  {
+    id: 2,
+    name: 'Simulation and Analysis',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg',
+    imageAlt:
+      'Olive drab green insulated bottle with flared screw lid and flat top.'
+  },
+  {
+    id: 3,
+    name: 'Waterjet cutting ',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg',
+    imageAlt:
+      'Person using a pen to cross a task off a productivity paper card.'
+  },
+  {
+    id: 4,
+    name: 'New Product Development',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+    imageAlt:
+      'Hand holding black machined steel mechanical pencil with brass tip and top.'
+  },
+  {
+    id: 5,
+    name: 'Machining',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+    imageAlt:
+      'Hand holding black machined steel mechanical pencil with brass tip and top.'
+  },
+  {
+    id: 6,
+    name: '3d printing ',
+    imageSrc:
+      'https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg',
+    imageAlt:
+      'Hand holding black machined steel mechanical pencil with brass tip and top.'
+  }
+]
+</script>
